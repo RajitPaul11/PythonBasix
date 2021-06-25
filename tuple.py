8015f77ade4b13ae79f6deeb635134afbc6f56b5
RajitPaul <rajitpaul09@gmail.com>
2021-06-25 14:30:41 +0530

    NewCommit

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#Creating a tuple
+t=(1,2,3,4,1,1,5)
+
+#Cannot update the value of tuple
+#Immutable
+
+#Accessing the value of tuple
+print(t[0])
+
+#Tuple Methods
+print(t.count(1))
+print(t.index(1))
+
+
+#Tuple with single element
+t=(1,)
+print(t)
